66053a4fc9231867ec388a8c0905c04e3f9f37fd
Raas <77680340+raas01@users.noreply.github.com>
2025-05-26 16:18:08 -0400

    create app shortcuts, hide other apps, single app  at a time for full focus

---
+++ init.lua
@@ -0,0 +1,55 @@
+-- List of hotkeys to focus app and hide others
+local optionKeys = {
+    a = "zen",
+    s = "Cursor",
+    d = "Slack",
+    f = "Figma",
+    o = "Obsidian",
+    z = "Finder",
+    g = "Google Chrome",
+  }
+
+  local cmdKeys = {
+    j = "ghostty"
+  }
+  
+  -- Focus app and hide all others
+  local function focusApp(appName)
+    -- Launch or focus the target app first
+    hs.application.launchOrFocus(appName)
+    
+    -- Then hide other apps in the background
+    local targetApp = hs.application.find(appName)
+    for _, app in ipairs(hs.application.runningApplications()) do
+      if app ~= targetApp and not app:isHidden() then
+        app:hide()
+      end
+    end
+    
+    layoutApp(appName)
+  end
+  
+  -- Bind keys (Hyper + key)
+  for key, app in pairs(optionKeys) do
+    hs.hotkey.bind({"option"}, key, function()
+      focusApp(app)
+    end)
+  end
+
+  for key, app in pairs(cmdKeys) do
+    hs.hotkey.bind({"cmd"}, key, function()
+      focusApp(app)
+    end)
+  end
+
+
+  hs.hotkey.bind({"option"}, "space", function()
+    local currentApp = hs.application.frontmostApplication()
+    for _, app in ipairs(hs.application.runningApplications()) do
+      if app ~= currentApp and not app:isHidden() then
+        app:hide()
+      end
+    end
+  end)
+
+  
